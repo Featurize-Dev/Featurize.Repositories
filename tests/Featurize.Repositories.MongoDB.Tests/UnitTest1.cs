9d90da169e5ac48f85e918667a0d2a7f807b302e
Patrick Evers <pmdevers@gmail.com>
2022-12-06 15:51:59 +0100

    Mongo Unit Tests

--- tests/Featurize.Repositories.MongoDB.Tests/UnitTest1.cs
+++ tests/Featurize.Repositories.MongoDB.Tests/UnitTest1.cs
@@ -1,29 +1,29 @@
+using EphemeralMongo;
 using Microsoft.Extensions.DependencyInjection;
-using Mongo2Go;
-using MongoDB.Driver.Core.Misc;
 
 namespace Featurize.Repositories.MongoDB.Tests;
 
 public class Tests
 {
-    internal static MongoDbRunner _runner;
-
     [SetUp]
     public void Setup()
     {
-        _runner = MongoDbRunner.Start();
+        
     }
 
     [Test]
     public async Task Test1()
     {
+        using var runner = MongoRunner.Run();
+
         var services = new ServiceCollection();
         var featureCollection = new FeatureCollection();
 
         featureCollection.AddRepositoryProvider(options =>
         {
-            options.UseMongo(_runner.ConnectionString);
-            options.AddRepository<Entity, Guid>(o => {
+            options.UseMongo(runner.ConnectionString);
+            options.AddRepository<Entity, Guid>(o =>
+            {
                 o.Database("Test");
                 o.CollectionName("TestCollection");
             });
@@ -34,7 +34,7 @@ public class Tests
             feature.Configure(services);
         }
 
-        var provider = services.BuildServiceProvider(); 
+        var provider = services.BuildServiceProvider();
 
         var repository = provider.GetRequiredService<IEntityRepository<Entity, Guid>>();
 
@@ -46,7 +46,7 @@ public class Tests
         await repository.SaveAsync(entity);
 
         var entity2 = await repository.FindByIdAsync(entity.Id);
-        
+
         Assert.Pass();
     }
 }
