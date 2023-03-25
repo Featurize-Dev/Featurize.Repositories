13ecc53d1e187362b5beaa07cf169a0b6cb806cb
Patrick Evers <pmdevers@gmail.com>
2023-03-25 01:45:54 +0100

    Publish Events on Save

--- src/Featurize.Repositories.Aggregates/AggregateRepositoryProvider.cs
+++ src/Featurize.Repositories.Aggregates/AggregateRepositoryProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Featurize.Repositories.Aggregates.Publsher;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Featurize.Repositories.Aggregates;
 internal class AggregateRepositoryProvider : IRepositoryProvider
@@ -7,10 +8,11 @@ internal class AggregateRepositoryProvider : IRepositoryProvider
 
     public string Name => DefaultName;
 
-    public bool IsConfigured => true;
+    public bool IsConfigured { get; }
 
     public void ConfigureProvider(IServiceCollection services)
     {
+        services.AddScoped<IEventPublisher, SimpleEventPublisher>();
     }
 
     public void ConfigureRepository(IServiceCollection services, RepositoryInfo info)
